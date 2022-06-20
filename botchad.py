bbaf4bd37d43e4b0b955c7ab60560e3d2db97594
preddy021 <preddy021@student.wethinkcode.co.za>
2022-06-20 22:36:37 +0200

    Added embed for stats, getting icon and champ image

--- botchad.py
+++ botchad.py
@@ -1,8 +1,8 @@
+from datapipelines import NotFoundError
 import discord
 from discord.ext import commands
 import cassiopeia as cass
 from league import League
-
 
 with open('config.txt', 'r') as f:
     tokens = f.readlines()
@@ -29,28 +29,37 @@ async def last(ctx, summ_name, region):
     await send_stats(ctx, summ_name, region)
 
 async def send_stats(ctx, summoner_name, region):
-    #TODO: Try except for KeyErro for region, sends invalid region if thrown
-    #TODO: Try except for NotFoundError for summoner_name, sends invalid summoner name if thrown
-    # if "league" in ctx.name:
     league = League()
-    match = league.get_latest_normal_match(summoner_name=summoner_name, region=region)
+    try:
+        match = league.get_latest_normal_match(summoner_name=summoner_name, region=region)
+    except NotFoundError as nfe:
+        embed = discord.Embed(title="Summoner not found :(")
+        embed.set_image(url="https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif")
+        await ctx.send(embed=embed)
+        return
+    except KeyError as ke:
+        embed = discord.Embed(title="That's a wacky region you've entered there...", description="Even the Taliyah doesn't know where that place is!")
+        embed.set_image(url="https://media.giphy.com/media/EjgA32SgtLpYAz2ZfB/giphy-downsized-large.gif")
+        await ctx.send(embed=embed)
+        return
 
     player = None
 
     for p in match.participants:
         if p.summoner.name == summoner_name:
             player = p
-
-    message = f"""Your stats from the last normal game: \n
-    Summoner: {summoner_name}\n
+    #TODO:Get op.gg profile using region and summoner name for set_author
+    # ^ op.gg/summoners/euw/Bodiez
+    embed = discord.Embed(title="Latest normal match", description=f"""
     Champion: {player.champion.name}\n
     Kills: {player.stats.kills}\n
     Deaths: {player.stats.deaths}\n
     Assists: {player.stats.assists}\n
-        """
+        """)
+    embed.set_author(name=summoner_name, icon_url=player.summoner.profile_icon.url)
+    embed.set_image(url=player.champion.image.url)
 
-    await ctx.send(message)
-
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
 
