0ff0e855e1f333db007b1e764565018a8b585685
preddy021 <preddy021@student.wethinkcode.co.za>
2022-06-22 19:45:28 +0200

    added support for different Queues

--- botchad.py
+++ botchad.py
@@ -2,6 +2,7 @@ from datapipelines import NotFoundError
 import discord
 from discord.ext import commands
 import cassiopeia as cass
+from cassiopeia import Queue
 from league import League
 
 with open('config.txt', 'r') as f:
@@ -25,21 +26,40 @@ async def on_ready():
     
 
 @bot.command(name="last")
-async def last(ctx, summ_name, region, match_type = None):
+async def last(ctx, summ_name=None, region=None, queue_type = None):
     #TODO: add support for full or simple
     #Full = Full match stats (all summoners)
     #Simple = Simmilar to what we have now, just with more stats.
-    await send_stats(ctx, summ_name, region, match_type)
 
-async def send_stats(ctx, summoner_name, region, match_type):
+    if summ_name == None and region == None:
+        embed = discord.Embed(title="Not enough arguments provided",
+        description= """
+    You left out your summoner name and region :( 
+
+    Correct format: **!!last [summoner_name] [region]**""")
+        await ctx.send(embed=embed)
+        return
+    elif region == None:
+        embed = discord.Embed(title="You left out the region!", description="Correct format: **!!last [summoner_name] [region]**")
+        await ctx.send(embed=embed)
+        return
+
+    await ctx.send("""
+Getting the data from your last match.
+One moment please... :clock:""")
+    await send_stats(ctx, summ_name, region, queue_type)
+
+async def send_stats(ctx, summoner_name, region, queue_type):
     league = League()
 
-    #TODO: Check if match_type is none/valid.
-    #Try/Except KeyError using new get/set queue_type
+    try:
+        queue: cass.Queue = League.get_queue_from_str(queue=queue_type)
+    except KeyError as ke:
+        queue = cass.Queue.normal_draft_fives
 
     try:
         #Parse through queue type here as param. 
-        match = league.get_latest_normal_match(summoner_name=summoner_name, region=region)
+        match = league.get_latest_match(summoner_name=summoner_name, region=region, queue=queue)
     except NotFoundError as nfe:
         embed = discord.Embed(title="Summoner not found :(")
         embed.set_image(url="https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif")
@@ -56,16 +76,28 @@ async def send_stats(ctx, summoner_name, region, match_type):
     for p in match.participants:
         if p.summoner.name == summoner_name:
             player = p
+    
+
+    if queue == Queue.ranked_flex_fives:
+        rank = player.summoner.ranks[Queue.ranked_flex_fives]
+        lp = player.summoner.league_entries.flex.league_points
+    else:
+        rank = player.summoner.ranks[Queue.ranked_solo_fives]
+        lp = player.summoner.league_entries.fives.league_points
+
+    queue_str = league.get_str_from_queue(queue=queue)
+
     #TODO:Get op.gg profile using region and summoner name for set_author
     # ^ op.gg/summoners/euw/Bodiez
-    embed = discord.Embed(title="Latest normal match", description=f"""
-    Champion: {player.champion.name}\n
-    Kills: {player.stats.kills}\n
-    Deaths: {player.stats.deaths}\n
-    Assists: {player.stats.assists}\n
+    embed = discord.Embed(title=f"Latest {queue_str} match", description=f"""
+    **Champion**: {player.champion.name}\n
+    **Kills**: {player.stats.kills}\n
+    **Deaths**: {player.stats.deaths}\n
+    **Assists**: {player.stats.assists}\n
         """)
     embed.set_author(name=summoner_name, icon_url=player.summoner.profile_icon.url)
     embed.set_image(url=player.champion.image.url)
+    embed.set_footer(text=f"Level: {player.summoner.level} || Solo/Duo Rank: {rank.tier} {rank.division} {lp}LP")
 
     await ctx.send(embed=embed)
 
