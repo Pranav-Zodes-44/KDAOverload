594b8226090861af97938221371916c64ef84401
preddy021 <preddy021@student.wethinkcode.co.za>
2022-06-27 14:21:24 +0200

    Added match_history command function. Changend send_stats to send_stats_simple. Added brief for last command. Added invalid_region function that returns sends the invalid regions as an embed

--- botchad.py
+++ botchad.py
@@ -25,11 +25,24 @@ async def on_ready():
     print(f"{guild.name} ID: {guild.id}")
     
 
-@bot.command(name="last")
+@bot.command(name="match_history", brief = "Shows your last 10 matches played. Defaulted to normal draft.")
+async def match_history():
+    league = League()
+
+    matches = league.get_match_history()
+
+    
+
+    return
+
+@bot.command(name="last", brief = "Shows your last match played. Defaulted to normal draft.", 
+            description="Shows your last match played. Defaulted to normal draft.\n Queue types: ranked/solo/duo, flex, normal/draft, aram, clash.")
 async def last(ctx, summ_name=None, region=None, queue_type = None):
+
     #TODO: add support for full or simple
     #Full = Full match stats (all summoners)
     #Simple = Simmilar to what we have now, just with more stats.
+
     if summ_name == None and region == None:
         embed = discord.Embed(title="Not enough arguments provided",
         description= """
@@ -40,24 +53,20 @@ async def last(ctx, summ_name=None, region=None, queue_type = None):
         return
     elif region == None:
         embed = discord.Embed(title="You left out the region!", description="Correct format: **!!last [summoner_name] [region]**")
-        await ctx.send(embed=embed)
+        await invalid_region(ctx, League(), embed)
         return
 
     await ctx.send("""
 Getting the data from your last match.
 One moment please... :clock:""")
-    await send_stats(ctx, summ_name, region, queue_type)
+    await send_stats_simple(ctx, summ_name, region, queue_type)
 
-async def send_stats(ctx, summoner_name, region, queue_type):
+async def send_stats_simple(ctx, summoner_name, region, queue_type):
     league = League()
 
-    try:
-        queue: cass.Queue = League.get_queue_from_str(queue=queue_type)
-    except KeyError as ke:
-        queue = cass.Queue.normal_draft_fives
+    queue: cass.Queue = League.get_queue_from_str(queue=queue_type)
 
     try:
-        #Parse through queue type here as param. 
         match = league.get_latest_match(summoner_name=summoner_name, region=region, queue=queue)
     except NotFoundError as nfe:
         embed = discord.Embed(title="Summoner not found :(")
@@ -65,40 +74,61 @@ async def send_stats(ctx, summoner_name, region, queue_type):
         await ctx.send(embed=embed)
         return
     except KeyError as ke:
-        embed = discord.Embed(title="That's a wacky region you've entered there...", description="Even the Taliyah doesn't know where that place is!")
-        embed.set_image(url="https://media.giphy.com/media/EjgA32SgtLpYAz2ZfB/giphy-downsized-large.gif")
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title="That's a wacky region you've entered there...", 
+                description="Even the Taliyah doesn't know where that place is!")
+        await invalid_region(ctx, league, embed)
         return
 
-    player = None
-
-    for p in match.participants:
-        if p.summoner.name == summoner_name:
-            player = p
-    
-
-    if queue == Queue.ranked_flex_fives:
-        rank = player.summoner.ranks[Queue.ranked_flex_fives]
-        lp = player.summoner.league_entries.flex.league_points
-    else:
-        rank = player.summoner.ranks[Queue.ranked_solo_fives]
-        lp = player.summoner.league_entries.fives.league_points
+    player = league.get_player_from_match(match=match, summoner_name=summoner_name)
 
     queue_str = league.get_str_from_queue(queue=queue)
     
+    embed = get_embed_last(queue_str, player, league)
+    
+    embed.set_footer(text=get_footer_text(queue=queue, player=player))
+    
+    await ctx.send(embed=embed)
+
+
+async def invalid_region(ctx, league: League, embed):
+    embed.add_field(name="Regions", value=league.get_regions(), inline=False)
+    embed.set_image(url="https://media.giphy.com/media/EjgA32SgtLpYAz2ZfB/giphy-downsized-large.gif")
+    await ctx.send(embed=embed)
+
+def get_embed_last(queue_str, player, league: League):
+
+    opgg = f"https://op.gg/summoners/{league.region.lower()}/{player.summoner.name}"
+
     embed = discord.Embed(title=f"Latest {queue_str} match", description=f"""
     **Champion**: {player.champion.name}\n
     **Kills**: {player.stats.kills}\n
     **Deaths**: {player.stats.deaths}\n
     **Assists**: {player.stats.assists}\n
         """)
-    embed.set_author(name=summoner_name, icon_url=player.summoner.profile_icon.url, 
-                        url=f"https://op.gg/summoners/{league.region.lower()}/{summoner_name}")
+    embed.set_author(name=player.summoner.name, icon_url=player.summoner.profile_icon.url, 
+                        url=opgg)
     embed.set_image(url=player.champion.image.url)
-    embed.set_footer(text=f"Level: {player.summoner.level} || Solo/Duo Rank: {rank.tier} {rank.division} {lp}LP")
-    
-    await ctx.send(embed=embed)
 
+    return embed
+
+
+def get_footer_text(queue: Queue, player):
+    try:
+        if queue == Queue.ranked_flex_fives:
+            rank = player.summoner.ranks[Queue.ranked_flex_fives]
+            lp = player.summoner.league_entries.flex.league_points
+            return f"Level: {player.summoner.level} || Flex Rank: {rank.tier} {rank.division} {lp}LP"
+        else:
+            rank = player.summoner.ranks[Queue.ranked_solo_fives]
+            lp = player.summoner.league_entries.fives.league_points
+            return f"Level: {player.summoner.level} || Solo/Duo Rank: {rank.tier} {rank.division} {lp}LP"
+    except KeyError as ke:
+        if Queue.ranked_flex_fives in player.summoner.ranks.keys():
+            rank = player.summoner.ranks[Queue.ranked_flex_fives]
+            lp = player.summoner.league_entries.flex.league_points
+            return f"Level: {player.summoner.level} || Flex Rank: {rank.tier} {rank.division} {lp}LP"
+        else:
+            return f"Level: {player.summoner.level} || Unranked"
 
 #TODO: Brainstorm more commands
 
